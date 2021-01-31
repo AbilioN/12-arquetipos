b6118822d5a95af7cee8107e188b020bfed1c652
Colais <nicholasbalby@hotmail.com>
2021-01-31 14:32:51 -0300

    Cards renderizados via JS

---
+++ select.js
@@ -0,0 +1,319 @@
+let json = [
+    {
+        "ID": 1,
+        "QUESTION": "Busco melhoria contínua"
+    },
+    {
+        "ID": 2,
+        "QUESTION": "Sou bom(a) conselheiro(a)"
+    },
+    {
+        "ID": 3,
+        "QUESTION": "Vivo plenamente"
+    },
+    {
+        "ID": 4,
+        "QUESTION": "Prefiro dar a receber"
+    },
+    {
+        "ID": 5,
+        "QUESTION": "Não consigo dizer não"
+    },
+    {
+        "ID": 6,
+        "QUESTION": "Procuro ser sempre direto(a) e pontual"
+    },
+    {
+        "ID": 7,
+        "QUESTION": "Vou fundo em minhas convicções"
+    },
+    {
+        "ID": 8,
+        "QUESTION": "Sinto-me em segurança neste mundo"
+    },
+    {
+        "ID": 9,
+        "QUESTION": "Disciplina é a chave para o sucesso"
+    },
+    {
+        "ID": 10,
+        "QUESTION": "Tenho boas ideias, mas não as coloco em prática por falta de tempo"
+    },
+    {
+        "ID": 11,
+        "QUESTION": "Sou uma pessoa calma. Sou tranquilo(a)"
+    },
+    {
+        "ID": 12,
+        "QUESTION": "Um pouquinho de caos não faz mal a ninguém"
+    },
+    {
+        "ID": 13,
+        "QUESTION": "O ser humano é maravilhoso"
+    },
+    {
+        "ID": 14,
+        "QUESTION": "Sinto-me bem unindo pessoas"
+    },
+    {
+        "ID": 15,
+        "QUESTION": "Enxergo a verdadeira imagem por trás das aparências"
+    },
+    {
+        "ID": 16,
+        "QUESTION": "Em meio ao tédio busco fazer mudanças bruscas"
+    },
+    {
+        "ID": 17,
+        "QUESTION": "Não faço julgamento em minhas análises pessoais"
+    },
+    {
+        "ID": 18,
+        "QUESTION": "Busco sempre melhorar a minha vida"
+    },
+    {
+        "ID": 19,
+        "QUESTION": "Consigo exercer papel de líder"
+    },
+    {
+        "ID": 20,
+        "QUESTION": "Às vezes consigo ser eficiente sem me esforçar demais"
+    },
+    {
+        "ID": 21,
+        "QUESTION": "Fui uma criança, malcuidada, desleixada e iludida"
+    },
+    {
+        "ID": 22,
+        "QUESTION": "Fico feliz quando consigo mudar uma situação"
+    },
+    {
+        "ID": 23,
+        "QUESTION": "Incentivo o desenvolvimento das pessoas e da natureza"
+    },
+    {
+        "ID": 24,
+        "QUESTION": "Sinto-me uma pessoa atraente"
+    },
+    {
+        "ID": 25,
+        "QUESTION": "Minha vida muda de acordo com meus pensamentos"
+    },
+    {
+        "ID": 26,
+        "QUESTION": "Acredito que podemos enxergar as coisas por várias perspectivas"
+    },
+    {
+        "ID": 27,
+        "QUESTION": "Sou uma pessoa segura"
+    },
+    {
+        "ID": 28,
+        "QUESTION": "Esperei demais de mim mesmo"
+    },
+    {
+        "ID": 29,
+        "QUESTION": "Sinto-me bem quando faço as pessoas se divertirem"
+    },
+    {
+        "ID": 30,
+        "QUESTION": "A hostilidade não me intimida"
+    },
+    {
+        "ID": 31,
+        "QUESTION": "Tenho a tendência de confiar nas pessoas de imediato"
+    },
+    {
+        "ID": 32,
+        "QUESTION": "Busco acalmar os ânimos em busca de um bem comum"
+    },
+    {
+        "ID": 33,
+        "QUESTION": "Gosto de comandar"
+    },
+    {
+        "ID": 34,
+        "QUESTION": "Apego-me à espiritualidade para conseguir realizar minhas tarefas"
+    },
+    {
+        "ID": 35,
+        "QUESTION": "Há um mundo melhor a minha espera"
+    },
+    {
+        "ID": 36,
+        "QUESTION": "Minhas palavras ou ações fazem com que os outros se sintam humilhados"
+    },
+    {
+        "ID": 37,
+        "QUESTION": "Enxergo a vida como uma sequência de infortúnios e dor"
+    },
+    {
+        "ID": 38,
+        "QUESTION": "Não me reconheço mais"
+    },
+    {
+        "ID": 39,
+        "QUESTION": "Sinto-me intimidado(a) por pessoas em posição liderança"
+    },
+    {
+        "ID": 40,
+        "QUESTION": "Penso antes nos outros e depois em mim mesmo(a)"
+    },
+    {
+        "ID": 41,
+        "QUESTION": "Sou consistente com as ações que preciso executar para atingir meus objetivos"
+    },
+    {
+        "ID": 42,
+        "QUESTION": "Faço para os outros, mas não consigo fazer para mim"
+    },
+    {
+        "ID": 43,
+        "QUESTION": "Sinto prazer em cuidar dos outros"
+    },
+    {
+        "ID": 44,
+        "QUESTION": "Prezo pela minha autonomia e minha liberdade"
+    },
+    {
+        "ID": 45,
+        "QUESTION": "Não me isento de agir perante a uma injustiça"
+    },
+    {
+        "ID": 46,
+        "QUESTION": "Penso que nem todas as regras precisam ser cumpridas"
+    },
+    {
+        "ID": 47,
+        "QUESTION": "No mundo, tudo está conectado"
+    },
+    {
+        "ID": 48,
+        "QUESTION": "As pessoas me acham extrovertido(a)"
+    },
+    {
+        "ID": 49,
+        "QUESTION": "A inspiração vem facilmente para mim"
+    },
+    {
+        "ID": 50,
+        "QUESTION": "Sou sempre eu mesmo(a) independente de onde eu esteja"
+    },
+    {
+        "ID": 51,
+        "QUESTION": "Esforço-me para criar minha própria realidade"
+    },
+    {
+        "ID": 52,
+        "QUESTION": "Contribuir com outras pessoas me fez muito bem"
+    },
+    {
+        "ID": 53,
+        "QUESTION": "Sinto que as pessoas são boas e não têm maldade"
+    },
+    {
+        "ID": 54,
+        "QUESTION": "Sempre achei melhor amar e perder do que nunca ter amado"
+    },
+    {
+        "ID": 55,
+        "QUESTION": "A jornada é tão importante quanto o destino"
+    },
+    {
+        "ID": 56,
+        "QUESTION": "Sei que se precisar de cuidados, não estarei só"
+    },
+    {
+        "ID": 57,
+        "QUESTION": "Mudanças fazem-me sentir desorientado"
+    },
+    {
+        "ID": 58,
+        "QUESTION": "Sinto-me como se estivesse sido deixado(a) para trás"
+    },
+    {
+        "ID": 59,
+        "QUESTION": "Consigo distribuir as atividades certas para pessoas certas com base em suas habilidades"
+    },
+    {
+        "ID": 60,
+        "QUESTION": "Sou uma pessoa que mantém o olhar voltado para o futuro"
+    },
+    {
+        "ID": 61,
+        "QUESTION": "Minha realidade é a materialização do meu sonho"
+    },
+    {
+        "ID": 62,
+        "QUESTION": "Tenho necessidade de algum tipo de rompimento"
+    },
+    {
+        "ID": 63,
+        "QUESTION": "Sinto-me destemido(a) diante de um desafio e não temo em realizá-lo(a)"
+    },
+    {
+        "ID": 64,
+        "QUESTION": "Fui decepcionado(a) por pessoas que eram importantes em minha vida"
+    },
+    {
+        "ID": 65,
+        "QUESTION": "Estar bem e saudável faz com que eu sinta mais disposto(a) no processo de cura dos outros"
+    },
+    {
+        "ID": 66,
+        "QUESTION": "Fui deixado para trás pelas pessoas que mais confiei"
+    },
+    {
+        "ID": 67,
+        "QUESTION": "Encontro completude nas minhas relações pessoais"
+    },
+    {
+        "ID": 68,
+        "QUESTION": "Algo me incomoda"
+    },
+    {
+        "ID": 69,
+        "QUESTION": "Por vezes, minha presença em um ambiente contribui para que mudanças aconteçam"
+    },
+    {
+        "ID": 70,
+        "QUESTION": "Estou em processo de desapego de coisas que já não fazem mais sentido em minha vida"
+    },
+    {
+        "ID": 71,
+        "QUESTION": "Gosto de colocar sorriso no rosto das pessoas mais improváveis"
+    },
+    {
+        "ID": 72,
+        "QUESTION": "Acredito que terei sempre tudo que me for necessário"
+    }
+];
+
+$(document).ready(function () {
+    console.log(Object.keys(json).length)
+    let container_all_questions = document.getElementById("container_cards")
+
+    function criarPergunta() {
+        let pergunta_container = document.createElement("div")
+        pergunta_container.className = 'pergunta_opcoes'
+        pergunta_container.appendChild(document.createElement("span"))
+        pergunta_container.appendChild(document.createElement("div"))
+        container_all_questions.appendChild(pergunta_container)
+    }
+
+
+    for (let i = 0; i < 5; i++) {
+        criarPergunta()
+    }
+
+
+
+    let lista_perguntas = []
+    /*
+    for (let i = 0; i < 3; i++) {
+        lista_perguntas[i] = container_select[i].appendChild(document.createElement("div"))
+
+    }
+    */
+
+})
